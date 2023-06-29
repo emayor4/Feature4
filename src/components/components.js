eaf36508fe85f59b3e45a36f982b9ae1a23ff7e5
NeolGG <neolgutierrez@gmail.com>
2023-06-28 22:46:22 -0400

    removed main, added components

---
+++ src/components/components.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Communities from './communitites/Communities'
+import Friends from './friends/Friends'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+export default function components() {
+  return (
+    <div>components</div>
+  )
+}
