0d227934196be2ff87208c1485738bace6ee782e
NeolGG <neolgutierrez@gmail.com>
2023-06-28 21:53:45 -0400

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./components/main/Main";
 import Friends from "./components/friends/Friends";
 import Communities from "./components/communitites/Communities";
 
 function App() {
   return (
+    <html>
+      hello
     <Router>
       <Routes>
         <Route path="/Main" element={<Main />} />
@@ -13,6 +15,8 @@ function App() {
         <Route path="/communities" element={<Communities />} />
       </Routes>
     </Router>
+    </html>
+
   );
 }
 
